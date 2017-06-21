198f8661b45fa24b5201e8671187aa966abb8f0b
N68G\Ekel <harlekin67@gmx.de>
2017-06-21 18:50:07 +0200

    no message

--- files/includes/bbcodes/img_bbcode_include.php
+++ files/includes/bbcodes/img_bbcode_include.php
@@ -18,27 +18,13 @@
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 
 if (!function_exists("img_bbcode_callback")) {
-	function img_bbcode_callback($matches) 
-	{
-		if (substr($matches[3], -1, 1) != "/") 
-		{
-			$avdata = @getimagesize("$matches[1]$matches[3]$matches[4]");
-			if (isset($avdata["mime"]))
-			{
-				return "<span style='display: block; max-width: 300px; max-height: 300px; overflow: auto;' class='forum-img-wrapper'><img src='".$matches[1].str_replace(array("?","&amp;","&","="), "", $matches[3]).$matches[4]."' alt='".$matches[3].$matches[4]."' style='border:0px' class='forum-img' /></span>";
-			}
-			else
-			{
-				return "[img]$matches[1]$matches[3]$matches[4][/img] is not a valid Image.";
-			}
+	function img_bbcode_callback($matches) {
+		if (substr($matches[3], -1, 1) != "/") {
+			return "<span style='display: block; max-width: 468px; max-height: 300px; overflow: auto;' class='forum-img-wrapper'><img src='".$matches[1].str_replace(array("?","&amp;","&","="), "", $matches[3]).$matches[4]."' alt='".$matches[3].$matches[4]."' style='border:0px' class='forum-img' /></span>";
+		} else {
+			return $matches[0];
 		}
-		else 
-		{
-			return "[img]$matches[1]$matches[3]$matches[4][/img] is not a valid Image.";
-		}
-		// End of BBCode IMG tag patch by Euforia33
 	}
-
 }
 	
 $text = preg_replace_callback("#\[img\]((http|ftp|https|ftps)://)(.*?)(\.(jpg|jpeg|gif|png|JPG|JPEG|GIF|PNG))\[/img\]#si", "img_bbcode_callback", $text);
